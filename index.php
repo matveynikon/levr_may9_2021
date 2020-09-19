86228aa5c44612e81476ebfff55bb32998d243b7
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-19 15:05:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+<p>...matthew..</p>
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);  
+  exec("npm install");
+  exec("node 2.\ search/amazon.js",$o,$v);
+  print_r($o);
+  echo($v);
+?>
+</body>
+</head>
