f7ac72b7142712226728c4469c8bbf7b1c07f15a
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-19 17:05:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   error_reporting(E_ALL);
   ini_set('display_errors', 1);  
   ignore_user_abort(true);   
-  //exec("npm install");
+  exec("npm install");
   exec("node 2.\ search/amazon.js",$o,$v);
   print_r($o);
   echo($v);
