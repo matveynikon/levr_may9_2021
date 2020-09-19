f37d8c85cda9b22babc20e97d37f5fd14a25f0f1
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-19 15:55:23 +0200

    Update amazon.js

--- 2. search/amazon.js
+++ 2. search/amazon.js
@@ -1,16 +1,7 @@
-/**
- * @name Amazon search
- *
- * @desc Looks for a "nyan cat pullover" on amazon.com, goes two page two clicks the third one.
- */
-const puppeteer = require('puppeteer')
-try {
-  (async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
-}
-catch(error){
-   console.log("no");
-}
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1280, height: 800 })
+  await page.goto('https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
+})
