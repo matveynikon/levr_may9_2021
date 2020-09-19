3842bda687bc84c8a212ad0bb9a471635e26fb21
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-19 16:43:58 +0200

    Update amazon.js

--- 2. search/amazon.js
+++ 2. search/amazon.js
@@ -1,7 +1,23 @@
 const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
-})
+var k = 1;
+var z = 0;
+var v = 2000;
+var x = Math.floor(Math.random() * 6000 + 105000);
+var j = 86400000 / (v) - x;
+var l = x / 14000;
+var k = 1;
+async function start() {
+  const url = 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean';
+  const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+
+  });
+  for(var g = 3000; g > 0; g--){
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 700});
+    await page.waitFor(5000);
+  }
+}
+start();
