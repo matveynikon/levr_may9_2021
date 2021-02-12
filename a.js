ea7028db6d868af41a33ecd19d5d70c329685ed4
Matthew <matvey.nikon@gmail.com>
2021-02-12 17:21:19 +0100

    make it better

--- a.js
+++ a.js
@@ -3,6 +3,7 @@ var n = 0;
 var k = 162;
 async function start() {
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -17,10 +18,6 @@ async function start() {
   console.log('hellloooo');
   await page.goto('https://www.instagram.com/funnymemes/followers/');    
   await page.waitFor(3000);
-  await page.screenshot({                      // Screenshot the website using defined options
- 
-    path: "./screenshot.png"                 // Save the screenshot in current directory
-  })
   try{
     await page.click('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.bIiDR')
   }
@@ -50,33 +47,90 @@ async function start() {
   var v10;*/
   var varlist = []
   let g = 0;
+  let url = 0;
   for (var c = 0; c < 50; c++){
-    urlist = ['https://www.instagram.com/funnymemes/followers/', 'https://www.instagram.com/memezar/followers/', 'https://www.instagram.com/memeselysium/followers/', 'https://www.instagram.com/meme.ig/followers/', 'https://www.instagram.com/swagaliciousjokes/followers/', 'https://www.instagram.com/meme.burn/followers/', 'https://www.instagram.com/dankgary/followers/', 'https://www.instagram.com/funnyvideos/followers/']
-    const url = Math.floor(Math.random() * 7 + 0)
-    let time = Math.floor(Math.random() * 10000 + 50000)
+    urlist = ['https://www.instagram.com/hb_dawgs/followers/', 'https://www.instagram.com/onlysarcasm.ig/followers/', 'https://www.instagram.com/amateurbantermemes/followers/', 'https://www.instagram.com/yizzerk/followers/', 'https://www.instagram.com/thememelogistics/followers/', 'https://www.instagram.com/legendarymemeguyyy/followers/', 'https://www.instagram.com/meemchamber/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/mcmemes22/followers/', 'https://www.instagram.com/imjustlivinmemes/followers/', 'https://www.instagram.com/lordmemerhere/followers/', 'https://www.instagram.com/memesac.2/followers/', 'https://www.instagram.com/meme_monkey_420/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/ohmyrickk/followers/', 'https://www.instagram.com/pubity_comedy/followers/', 'https://www.instagram.com/godssmemess/followers/', 'https://www.instagram.com/vabeif/followers/',
+    'https://www.instagram.com/issamemelover/followers/', 'https://www.instagram.com/staywithmecrox/followers/', 'https://www.instagram.com/memes_4yaall/followers/', 'https://www.instagram.com/drunken.fruit.fly/followers/', 'https://www.instagram.com/memehxstler/followers/', 'https://www.instagram.com/dogeoncrack/followers/', 'https://www.instagram.com/instant.memz/followers/', 'https://www.instagram.com/ultrameems/followers/', 'https://www.instagram.com/i.shidded.my.pants/followers/', 'https://www.instagram.com/fuckkcomedy/followers/', 'https://www.instagram.com/toiletmemes33/followers/', 'https://www.instagram.com/spongedome/followers/', 'https://www.instagram.com/siiiiiiick/followers/', 'https://www.instagram.com/saucynade/followers/', 'https://www.instagram.com/memestochill/followers/',
+    'https://www.instagram.com/dankgary/followers/', 'https://www.instagram.com/ofhensive/followers/', 'https://www.instagram.com/kurxie/followers/', 'https://www.instagram.com/awkwardiety/followers/', 'https://www.instagram.com/basictor/followers/', 'https://www.instagram.com/memestochill/followers/', 'https://www.instagram.com/heavy.pdf/followers/']
+    //urlist = ['']
+    //console.log(urlist.length)
+    let time = Math.floor(Math.random() * 14000 + 21000)
     await page.waitFor(time)
-    for (var z = 0; z < 8; z++){
-      await page.goto(urlist[url])
+    if(url == 39){
+      url = 0;
+    }
+    else{
       console.log(url)
+    }
+    for (var z = 0; z < 13; z++){
+      n = 0;
+      await page.goto(urlist[url])
+      console.log(urlist[url])
       await page.waitFor(2000)
-      await page.click('#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')
-      //#f24050daec6c098 > div > div > span > a
-      /*if(g == 0){
-        await page.waitFor(5000);
-        await page.mouse.click(410, 500)
-        await page.mouse.down(410, 500)
-        await page.mouse.move(410, 600)
-        await page.mouse.down(410, 600)
-        let g = 0;
+      try{
+        await page.click('#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')
       }
-      g += 1;*/
+      catch{
+        console.log('follow list already displayed')
+      }
+      //#f24050daec6c098 > div > div > span > a
+      /*if(z == 1){
+        await page.waitFor(5000);
+        await page.mouse.click(400, 400)
+        await page.mouse.down(400, 400)
+        await page.mouse.move(400, 450)
+        //await page.mouse.down(410, 600)
+      }*/
       await page.waitFor(8000)
+      if(z > 1){
+        await page.mouse.click(400, 400)
+        await page.waitFor(2000)
+        await page.keyboard.type(' ')
+        await page.waitFor(2000)
       //await page.click('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(1) > div > div.Igw0E.IwRSH.eGOV_._4EzTm.yC0tu > div > div > a > img')
       //await page.mouse.click(295, k)
-      const elements = await page.$x('/html/body/div[4]/div/div/div[2]/ul/div/li/div/div[2]/div[1]/div/div/span')
       ///html/body/div[4]/div/div/div[2]/ul/div/li[7]/div/div[1]/div[2]/div[1]/span
       ///html/body/div[4]/div/div/div[2]/ul/div/li[2]/div/div[2]/div[1]/div/div/span
-      await elements[z].click()   
+        //const elements = await page.$x('/html/body/div[4]/div/div/div[2]/ul/div/li/div/div[1]/div[2]/div[1]/span')
+      //await page.evaluate(() => document.querySelector(nine).scrollIntoView());
+      //await page.hover(nine);
+        z += 8
+        //await elements[z].click()
+        await page.waitFor(2000)
+      }
+      else{
+        console.log('still did not scroll')
+      }
+      if(z > 12){
+        await page.mouse.click(400, 400)
+        await page.waitFor(2000)
+        await page.keyboard.type(' ')
+        await page.waitFor(2000)
+      //await page.click('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(1) > div > div.Igw0E.IwRSH.eGOV_._4EzTm.yC0tu > div > div > a > img')
+      //await page.mouse.click(295, k)
+      ///html/body/div[4]/div/div/div[2]/ul/div/li[7]/div/div[1]/div[2]/div[1]/span
+      ///html/body/div[4]/div/div/div[2]/ul/div/li[2]/div/div[2]/div[1]/div/div/span
+        //const elements3 = await page.$x('/html/body/div[4]/div/div/div[2]/ul/div/li/div/div[1]/div[2]/div[1]/span')
+      //await page.evaluate(() => document.querySelector(nine).scrollIntoView());
+      //await page.hover(nine);
+        z += 7
+        //await elements3[z].click()
+        await page.waitFor(2000)
+      }
+      else{
+        console.log('still did not scroll')
+      }      
+      //nine.scrollIntoView();
+      //await nine.click()
+      try{
+        const elements2 = await page.$x('/html/body/div[4]/div/div/div[2]/ul/div/li/div/div[2]/div[1]/div/div/span')
+        await elements2[z].click()        
+      }   
+      catch{
+        let info = 'Finished loop early'
+        console.log('\x1b[36m%s\x1b[0m', info);
+        break;
+      }
       //await page.click('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(2) > div > div.Igw0E.IwRSH.eGOV_._4EzTm.yC0tu > div > div > span')
       k += 45;
       await page.waitFor(1000)
@@ -90,8 +144,10 @@ async function start() {
         var name = await page.evaluate(element => element.textContent, element);
         console.log(name)
       }
-      if(varlist.indexOf(name) > -1){
-        console.log(varlist)
+      var i = (varlist.indexOf(name));
+      console.log(i)
+      if(i > -1){
+          console.log('already there')
       }
       else{
         try{
@@ -102,17 +158,54 @@ async function start() {
           await page.waitFor(3000)
           await page.click('body > div._2dDPU.RnrQH.CkGkG > div.EfHg9 > div > div > a')
           await page.waitFor(4000)
-          await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
+          try{
+            await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
+          }
+          catch{
+            console.log('only one post')
+          }
+          try{
+            await page.waitFor(2000)
+            next = await page.waitForXPath('/html/body/div[4]/div[1]/div/div/a[2]')  
+            await next.click()    
+            await page.waitFor(4000)
+            try{
+                await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
+            }
+            catch{
+              console.log('only two posts')
+            }                    
+          }
+          catch{
+            console.log('only two posts')
+          }
+          try{
+            await page.waitFor(2000)
+            next = await page.waitForXPath('/html/body/div[4]/div[1]/div/div/a[2]')  
+            await next.click()    
+            await page.waitFor(4000)
+            try{
+              await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
+            }
+            catch{
+              console.log('only three posts')
+            }                    
+          }
+          catch{
+            console.log('only three posts')
+          }
         }
         catch{
+          n = 1;
           console.log('private account')
         }
       }
       await page.waitFor(2000)  
       //var array = [text, text, text, text, text]
-      varlist.push(name)
+      varlist.push(name + '   ' + n + 'list value' + ' ' + i)
       console.log(varlist)
     }
+    url += 1;
   }
 
 }
--- s.js
+++ s.js
@@ -3,6 +3,7 @@ var n = 0;
 var k = 162;
 async function start() {
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -48,20 +49,20 @@ async function start() {
   let g = 0;
   let url = 0;
   for (var c = 0; c < 50; c++){
-    urlist = ['https://www.instagram.com/hb_dawgs/followers/', 'https://www.instagram.com/onlysarcasm.ig/followers/', 'https://www.instagram.com/amateurbantermemes/followers/', 'https://www.instagram.com/yizzerk/followers/', 'https://www.instagram.com/thememelogistics/followers/', 'https://www.instagram.com/legendarymemeguyyy/followers/', 'https://www.instagram.com/meemchamber/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/mcmemes22/followers/', 'https://www.instagram.com/imjustlivinmemes/followers/', 'https://www.instagram.com/lordmemerhere/followers/', 'https://www.instagram.com/memesac.2/followers/', 'https://www.instagram.com/meme_monkey_420/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/ohmyrickk/followers/', 'https://www.instagram.com/pubity_comedy/followers/', 'https://www.instagram.com/godssmemess/followers/', 'https://www.instagram.com/vabeif/followers/',
+    /*urlist = ['https://www.instagram.com/hb_dawgs/followers/', 'https://www.instagram.com/onlysarcasm.ig/followers/', 'https://www.instagram.com/amateurbantermemes/followers/', 'https://www.instagram.com/yizzerk/followers/', 'https://www.instagram.com/thememelogistics/followers/', 'https://www.instagram.com/legendarymemeguyyy/followers/', 'https://www.instagram.com/meemchamber/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/mcmemes22/followers/', 'https://www.instagram.com/imjustlivinmemes/followers/', 'https://www.instagram.com/lordmemerhere/followers/', 'https://www.instagram.com/memesac.2/followers/', 'https://www.instagram.com/meme_monkey_420/followers/', 'https://www.instagram.com/justthings05/followers/', 'https://www.instagram.com/ohmyrickk/followers/', 'https://www.instagram.com/pubity_comedy/followers/', 'https://www.instagram.com/godssmemess/followers/', 'https://www.instagram.com/vabeif/followers/',
     'https://www.instagram.com/issamemelover/followers/', 'https://www.instagram.com/staywithmecrox/followers/', 'https://www.instagram.com/memes_4yaall/followers/', 'https://www.instagram.com/drunken.fruit.fly/followers/', 'https://www.instagram.com/memehxstler/followers/', 'https://www.instagram.com/dogeoncrack/followers/', 'https://www.instagram.com/instant.memz/followers/', 'https://www.instagram.com/ultrameems/followers/', 'https://www.instagram.com/i.shidded.my.pants/followers/', 'https://www.instagram.com/fuckkcomedy/followers/', 'https://www.instagram.com/toiletmemes33/followers/', 'https://www.instagram.com/spongedome/followers/', 'https://www.instagram.com/siiiiiiick/followers/', 'https://www.instagram.com/saucynade/followers/', 'https://www.instagram.com/memestochill/followers/',
-    'https://www.instagram.com/dankgary/followers/', 'https://www.instagram.com/ofhensive/followers/', 'https://www.instagram.com/kurxie/followers/', 'https://www.instagram.com/awkwardiety/followers/', 'https://www.instagram.com/basictor/followers/', 'https://www.instagram.com/memestochill/followers/', 'https://www.instagram.com/heavy.pdf/followers/']
-    //urlist = ['']
+    'https://www.instagram.com/dankgary/followers/', 'https://www.instagram.com/ofhensive/followers/', 'https://www.instagram.com/kurxie/followers/', 'https://www.instagram.com/awkwardiety/followers/', 'https://www.instagram.com/basictor/followers/', 'https://www.instagram.com/memestochill/followers/', 'https://www.instagram.com/heavy.pdf/followers/']*/
+    urlist = ['https://www.instagram.com/memezar/followers/', 'https://www.instagram.com/jokezar/followers/', 'https://www.instagram.com/britishmemes/followers/', 'https://www.instagram.com/meme.ig/followers/', 'https://www.instagram.com/funnymemes/followers/', 'https://www.instagram.com/puberty/followers/', 'https://www.instagram.com/leavelaughing/followers/']
     //console.log(urlist.length)
-    let time = Math.floor(Math.random() * 140000 + 210000)
-    await page.waitFor(time)
-    if(url == 39){
+    let time = Math.floor(Math.random() * 14000 + 21000)
+    //await page.waitFor(time)
+    if(url == 6){
       url = 0;
     }
     else{
       console.log(url)
     }
-    for (var z = 0; z < 8; z++){
+    for (var z = 0; z < 50; z++){
       n = 0;
       await page.goto(urlist[url])
       console.log(urlist[url])
@@ -82,14 +83,14 @@ async function start() {
         let g = 0;
       }
       g += 1;*/
-      await page.waitFor(8000)
+      await page.waitFor(3000)
       //await page.click('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(1) > div > div.Igw0E.IwRSH.eGOV_._4EzTm.yC0tu > div > div > a > img')
       //await page.mouse.click(295, k)
       const elements = await page.$x('/html/body/div[4]/div/div/div[2]/ul/div/li/div/div[2]/div[1]/div/div/span')
       ///html/body/div[4]/div/div/div[2]/ul/div/li[7]/div/div[1]/div[2]/div[1]/span
       ///html/body/div[4]/div/div/div[2]/ul/div/li[2]/div/div[2]/div[1]/div/div/span
       try{
-        await elements[z].click()        
+        await elements[0].click()        
       }   
       catch{
         let info = 'Finished loop early'
@@ -116,13 +117,13 @@ async function start() {
       }
       else{
         try{
-          await page.waitFor(3000)
+          await page.waitFor(1000)
           await page.click('#react-root > section > main > div > div._2z6nI > article > div > div > div > div:nth-child(1) > a > div.eLAPa')
-          await page.waitFor(5000)
+          await page.waitFor(1000)
           await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')
-          await page.waitFor(3000)
+          await page.waitFor(1000)
           await page.click('body > div._2dDPU.RnrQH.CkGkG > div.EfHg9 > div > div > a')
-          await page.waitFor(4000)
+          await page.waitFor(1000)
           try{
             await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
           }
@@ -130,10 +131,10 @@ async function start() {
             console.log('only one post')
           }
           try{
-            await page.waitFor(2000)
+            await page.waitFor(1000)
             next = await page.waitForXPath('/html/body/div[4]/div[1]/div/div/a[2]')  
             await next.click()    
-            await page.waitFor(4000)
+            await page.waitFor(1000)
             try{
                 await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
             }
@@ -145,10 +146,10 @@ async function start() {
             console.log('only two posts')
           }
           try{
-            await page.waitFor(2000)
+            await page.waitFor(1000)
             next = await page.waitForXPath('/html/body/div[4]/div[1]/div/div/a[2]')  
             await next.click()    
-            await page.waitFor(4000)
+            await page.waitFor(1000)
             try{
               await page.click('body > div._2dDPU.RnrQH.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > div > span > svg')      
             }
