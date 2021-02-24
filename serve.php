4c44dca594281e80dfd7a2bebb06c52582ae1a6d
Matthew <matvey.nikon@gmail.com>
2021-02-24 19:58:14 +0100

    make it better

--- serve.php
+++ serve.php
@@ -196,39 +196,71 @@ fwrite($myfile, $txt3);
 fwrite($myfile, $el);
 fwrite($myfile, $bro);
 fwrite($myfile, $el2);
-/*$txt = "async function start () {\n";
-fwrite($myfile, $txt);
-$txt = "  const browser = await puppeteer.launch({\n";
-fwrite($myfile, $txt);
-$txt = "    args: ['--no-sandbox']\n";
-fwrite($myfile, $txt);
-$txt = "  })\n";
-fwrite($myfile, $txt);
-$txt = "  for (x = 5; x > 0; x--) {\n";
-fwrite($myfile, $txt);
-$txt = "    const page = await browser.newPage()\n";
-fwrite($myfile, $txt);
-$txt = "    await page.goto(";
-fwrite($myfile, $txt);
-$txt = $_POST["name"];
-fwrite($myfile, $txt);
-$txt = ")\n";
-fwrite($myfile, $txt);
-$txt = "    await page.waitFor(5000)\n";
-fwrite($myfile, $txt);
-$txt = "    await page.close()\n";
-fwrite($myfile, $txt);
-$txt = "  }\n";
-fwrite($myfile, $txt);
-$txt = "  await browser.close()\n";
-fwrite($myfile, $txt);
-$txt = "}\n";
-fwrite($myfile, $txt);
-$txt = "start()\n";
-fwrite($myfile, $txt);*/
 fclose($myfile);
 sleep(3);
-exec("node puppet.js &", $output);
+/* An easy way to keep in track of external processes.
+* Ever wanted to execute a process in php, but you still wanted to have somewhat controll of the process ? Well.. This is a way of doing it.
+* @compability: Linux only. (Windows does not work).
+* @author: Peec
+*/
+class Process{
+    private $pid;
+    private $command = "node puppet.js";
+
+    public function __construct($cl=false){
+        if ($cl != false){
+            $this->command = $cl;
+            $this->runCom();
+        }
+    }
+    private function runCom(){
+        $command = 'nohup '.$this->command.' > /dev/null 2>&1 & echo $!';
+        exec($command ,$op);
+        $this->pid = (int)$op[0];
+    }
+
+    public function setPid($pid){
+        $this->pid = $pid;
+    }
+
+    public function getPid(){
+        return $this->pid;
+    }
+
+    public function status(){
+        $command = 'ps -p '.$this->pid;
+        exec($command,$op);
+        if (!isset($op[1]))return false;
+        else return true;
+    }
+
+    public function start(){
+        if ($this->command != '')$this->runCom();
+        else return true;
+    }
+
+    public function stop(){
+        $command = 'kill '.$this->pid;
+        exec($command);
+        if ($this->status() == false)return true;
+        else return false;
+    }
+}
+    // You may use status(), start(), and stop(). notice that start() method gets called automatically one time.
+$process = new Process('ls -al');
+
+    // or if you got the pid, however here only the status() metod will work.
+$process = new Process();
+$process.setPid(my_pid);
+    // Then you can start/stop/ check status of the job.
+    //$process.stop();
+    $process.start();
+if ($process.status()){
+  echo "The process is currently running";
+}else{
+  echo "The process is not running.";
+}
+getPid()
 ?>
 </body>
 </head>
